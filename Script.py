2e3ae88aefddf0ce8a2606b8d9115f7da7408a9b
ANGEL RAMIREZ GOMERO <angel.ramirez@unmsm.edu.pe>
2023-02-09 21:53:25 -0500

    Probando conexion con computadora 1

--- Script.py
+++ Script.py
@@ -1,8 +1,8 @@
 from Postgress.ClassPostgress import DataBasePostgres
 
 user="postgres"
-name_db="example_postgressql"
-#pythonname_db="postgres"
+name_db="postgres"
+pythonname_db="postgres"
 password="12345678"
 host="localhost"
 port="5432"
@@ -10,3 +10,5 @@ port="5432"
 
 database=DataBasePostgres(host=host, port=port,user=user, password=password, database_name=name_db)
 database.connect()
+
+print("probando establecer conexion")
